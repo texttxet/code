608646215d498cb84f9814d747200a75a3941c76
pigofsir <1115184995@qq.com>
2019-10-29 20:59:39 +0800

    ctf杂项一类

---
+++ misc/crc32碰撞/crc32碰撞.py
@@ -0,0 +1,28 @@
+import string
+import zipfile
+import binascii
+
+def jisuancrc(crc):
+	for a in dic:
+		for b in dic:
+			for c in dic:
+				for d in dic:
+					s = a + b + c + d
+					s = str(s).encode()
+					# print(type(s))
+					
+					if (binascii.crc32(s)) == crc:
+						print(s)
+						# print(binascii.crc32(s))
+						return
+
+def getcrc():
+	f = zipfile.ZipFile("flag.zip", "r")
+	GetCrc = f.getinfo("flag.txt")
+	crc = GetCrc.CRC
+	print(hex(crc))
+	jisuancrc(crc)
+
+dic = "abcdefghijklmnopqrst1234567890" # 可以自己自定义文件内容范围
+# dic = string.ascii_letters + string.digits # 直接应用模块来确定文件内容范围
+getcrc()
